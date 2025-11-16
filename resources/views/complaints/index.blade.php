ee54e747de700a3341741b04374657d3dc6cbb28
almerkilfergalleto <almerkilfergalleto868@gmail.com>
2025-11-16 20:17:32 +0800

    dominngo sa gabie

--- resources/views/complaints/index.blade.php
+++ resources/views/complaints/index.blade.php
@@ -102,7 +102,7 @@
                     @if(Auth::user()->role !== 'admin')
                         <a href="{{ route('complaints.create') }}"
                            class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 mb-4 inline-block">
-                            + New Complaint
+                            + New Report
                         </a>
                     @endif
 
